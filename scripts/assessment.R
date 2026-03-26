fd1bf405a35a635e42f9fa09b12ad827f4d1984a
Renato Vargas <renato@renatovargas.com>
2026-03-25 18:25:53 -0600

    Fixed navbar

--- scripts/assessment.R
+++ scripts/assessment.R
@@ -1,11 +1,14 @@
 library(tidyverse)
+library(readxl)
 library(openxlsx)
 
-ga2025 <- readxl::read_excel(
+# Read Assessment Data
+ga2025 <- read_excel(
   "data/assessment/global_assessment_2025.xlsx",
   sheet = "seea_ga_db_2025"
 )
 
+# Pivot Longer for Convenience
 ga2025long <- ga2025 |>
   pivot_longer(
     c(13:length(colnames(ga2025))),
@@ -29,8 +32,26 @@ afe2025 <- ga2025long |>
     notes = ""
   )
 
+# Extract the list of accounts
+seea_accounts <- read_excel(
+  "data/assessment/global_assessment_2025.xlsx",
+  sheet = "Cover",
+  range = "B23:C59"
+) |>
+  fill(`Thematic category`, .direction = "down")
+
+# Various saves
+saveRDS(
+  seea_accounts,
+  "outputs/assessment/seea_accounts.RDS"
+)
+saveRDS(
+  afe2025,
+  "outputs/assessment/AFE_assessment_2025.RDS"
+)
+
 write.xlsx(
   afe2025,
-  "outputs/AFE_assessment_2025.xlsx",
+  "outputs/assessment/AFE_assessment_2025.xlsx",
   overwrite = T
 )
